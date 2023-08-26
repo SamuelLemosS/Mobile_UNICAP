a89e52004dec3f3827204ea899441ac45ee8e3c4
SamuelLemosS <107142956+SamuelLemosS@users.noreply.github.com>
2023-08-26 00:38:50 +0000

    start

---
+++ listaTarefas/App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TextInput 
+        placeholder='Digite sua tarefa'
+        style={styles.input}
+      />
+      <Button
+        placeholder='Adicine a tarefa'
+        style={styles.teste}
+      />
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input:{
+    width: 200,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderBlockColor:"black"
+  },
+  teste:{
+    backgroundColor:"black",
+  },
+});
