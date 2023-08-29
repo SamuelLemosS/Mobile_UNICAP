a55ea00e320d2758a1e5946ac9a532b8873f0f73
SamuelLemosS <samuellemosesilva@gmail.com>
2023-08-28 22:48:08 -0300

    function finish

--- listaTarefas/App.js
+++ listaTarefas/App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { useEffect, useReducer, useState } from "react";
 
 export default function App() {
+  const [text, setText] = useState("");
+
+  function reducer(state, action) {
+    if (action.type === "addItem") {
+      return { ...state, data: [...state.data, text] };
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, { data: [] });
+
+  useEffect(() => {
+    console.log(state.data);
+  });
+
   return (
     <View style={styles.container}>
       <View style={styles.view}>
-        <TextInput 
-          placeholder='Digite sua tarefa'
+        <TextInput
+          onChangeText={setText}
+          placeholder="Digite sua tarefa"
           style={styles.input}
         />
-        <Button
-          placeholder='Adicine a tarefa'
-          style={styles.teste}
-        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => dispatch({ type: "addItem" })}
+        >
+          <Text>Adicionar</Text>
+        </TouchableOpacity>
       </View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={state.data}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item}</Text>
+          </View>
+        )}
+      />
+      <Text style={styles.text}>{state.data.length} Tareas</Text>
     </View>
   );
 }
@@ -23,22 +56,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  input:{
-    width: 200,
+  input: {
+    margin: 10,
+    width: 150,
+    height: 50,
     borderWidth: 1,
     borderRadius: 5,
-    borderBlockColor:"black"
+    borderBlockColor: "black",
+    padding: 5,
   },
-  teste:{
-    backgroundColor:"black",
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    width: 100,
+    height: 50,
+    borderRadius: 5,
+    padding: 5,
+    margin: 10,
   },
-  view:{
+  view: {
+    alignItems:'center',
+    justifyContent: 'space-between',
+    flexDirection: "row",
+    width: 300,
+    marginTop: 50,
     borderWidth: 1,
     borderRadius: 5,
-    borderBlockColor:"black"
-  }
+    borderBlockColor: "black",
+  },
+  text: {
+    marginBottom: 50,
+  },
 });
